020b38863093ab10fdfb10971156c6524fa33ca2
UMAID-AKHTAR <umaidakhtar933@gmail.com>
2026-01-17 16:08:04 +0530

    try catch block added + some tailwind css

--- src/Randompsswrd.jsx
+++ src/Randompsswrd.jsx
@@ -7,7 +7,7 @@ function Randompsswrd() {
   const [length, setLength] = useState(4)
   const [Upper, setUpper] = useState(false);
   const [Lower, setLower] = useState(false);
-  const [Number, setNumber] = useState(false);
+  const [number, setNumber] = useState(false);
   const [Symbol, setSymbol] = useState(false);
   const [fpPass, setfpPass] = useState("")
   
@@ -21,11 +21,16 @@ function Randompsswrd() {
 
     let final=""
     let ch=""
-    if( Upper || Lower || Number || Symbol)
+
+    try{ 
+    if (!length || length < 4 || length>15) {
+      throw new Error("Invalid password length");
+    }
+    if( Upper || Lower || number || Symbol)
     {
       if(Upper) ch+=uc;
       if(Lower) ch+=lc;
-      if(Number) ch+=nm;
+      if(number) ch+=nm;
       if(Symbol) ch+=sm;
 
       for(let i=0; i<length; i++)
@@ -38,8 +43,11 @@ function Randompsswrd() {
 
     else{
       alert('Plese Choose atleast One CheckBox');
+      }
     }
-
+    catch (error) {
+    alert(error.message);
+    }
   }
 
   const Copy=()=>{
@@ -52,8 +60,8 @@ function Randompsswrd() {
 
   return (
     <>
-     <div className="bg-blue-950 w-90 h-110 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <h1 className="text-red-900 text-center py-4 text-2xl font-bold hover:text-blue-50">Password Generator</h1>
+     <div className="bg-gradient-to-br from-blue-900 via-blue-700 to-sky-600 w-[360px] h-[440px] rounded-xl shadow-lg transition-all duration-300 ease-in-out  hover:shadow-[0_0_25px_rgba(255,255,255,0.9)] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <h1 className="text-black text-center py-4 text-2xl font-bold hover:text-blue-50">Password Generator</h1>
       
       <div className="ml-6 mt-6 mr-6  h-10 flex flex-row"> 
         <input
@@ -64,7 +72,7 @@ function Randompsswrd() {
         />
         <button 
          onClick={Copy}
-         className="bg-red-300 font-bold h-full px-5 rounded-r-md">Copy</button>
+         className="bg-red-300 text-amber-50 font-bold h-full px-5 rounded-r-md">Copy</button>
       </div>
 
       <div className="ml-6 mt-2 mr-6 font-bold h-10 flex flex-row py-2">
@@ -75,13 +83,14 @@ function Randompsswrd() {
           max={15}
           value={length}
           onChange={(e)=> setLength(Number(e.target.value))}
-         className="w-20  ml-auto h-full text-center bg-amber-50 px-3 outline-none"/>
+         className="w-20 text-black ml-auto h-full text-center bg-amber-50 px-3 outline-none"/>
       </div>
 
       <div className="ml-6 mt-2 mr-6  font-bold h-10 flex flex-row py-2">
         <span className="text-blue-50">Include Upper Case Letters</span>
         <input
          type="checkbox"
+         checked={Upper}
          onChange={(e)=>setUpper(e.target.checked)}
          className=" w-5 h-5 ml-auto"/>
       </div>
@@ -91,6 +100,7 @@ function Randompsswrd() {
         <span className="text-blue-50">Include Lower Case Letters</span>
         <input
          type="checkbox"
+         checked={Lower}
          onChange={(e)=>setLower(e.target.checked)}
          className=" w-5 h-5 ml-auto"/>
       </div>
@@ -100,6 +110,7 @@ function Randompsswrd() {
         <span className="text-blue-50">Include Number</span>
         <input
          type="checkbox"
+         checked={number}
          onChange={(e)=>setNumber(e.target.checked)}
          className=" w-5 h-5 ml-auto"/>
       </div>
@@ -108,13 +119,14 @@ function Randompsswrd() {
         <span className="text-blue-50">Include symbols</span>
         <input
          type="checkbox"
+         checked={Symbol}
          onChange={(e)=>setSymbol(e.target.checked)}
          className=" w-5 h-5 ml-auto"/>
       </div>
       
       <div className="text-center text-white font-bold ml-8 mt-2 mr-8 bg-black h-10 py-2 rounded hover:bg-blue-800 
           hover:shadow-[0_0_20px_rgba(255,255,255,0.25)]"
-          onClick={() => geneRate()}
+          onClick={geneRate}
           >Generate Password</div>
      </div>
 
